94ccb425ca01f83834ab17870085dc4ad3ccb0fb
Vivekanandpa <96114197+Vivekanandpa@users.noreply.github.com>
2021-12-15 10:07:08 +0530

    commiting login

---
+++ REST/api/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers, validators
+
+from api.models import Student
+
+class StudentSerializer(serializers.ModelSerializer):
+     class Meta:
+        model=Student
+        fields=['name','roll','city']
+        
